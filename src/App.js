3dd4afe60c3b0dade67ff7d41fdad605b11ba32a
ELMANBAKI <digitalelman@gmail.com>
2024-09-25 20:40:18 +0100

    test3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './Header';
 import Nav from './Nav';
+import Main from './Main';
 
 function App() {
   return (
     <>
       <Header />
       <Nav />
+      <Main />
+      
 
 
   </>
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon!</h2>
+      <p>Enjoy the best dishes and a great atmosphere.</p>
+      {/* يمكنك إضافة محتوى آخر هنا */}
+    </main>
+  );
+};
+
+export default Main;
