e423fd5e3b3a1d4475c0315efcb2443d8f160af0
ELMANBAKI <digitalelman@gmail.com>
2024-09-29 14:30:18 +0100

    test 5

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
-const Header = () => {
-    return (
-      <header>
-        <h1>Logo H1</h1>
-        <img src="/page/Logo.png" alt="" />
-        
-      </header>
-    );
-  };
-export default Header ;
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src="/page/Logo.png" className='App-logo' alt="logo"/>
+      <h1>Welcome to Little Lemon</h1>
+    </header>
+  );
+}
+export default Header;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import './App.css';
 
 const Nav = () => {
   return (
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="#home" className="App-link">Home</a></li>
+        <li><a href="#about" className="App-link">About</a></li>
+        <li><a href="#menu" className="App-link">Menu</a></li>
       </ul>
     </nav>
   );
